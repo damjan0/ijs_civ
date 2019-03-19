39815a5fb2d8fd36815fedaf1263346cccd320f7
Beno Šircelj <benosircelj@gmail.com>
2019-03-19 15:17:15 +0100

    N(L) all 3 dists

--- generateData3D_N(L).py
+++ generateData3D_N(L).py
@@ -9,21 +9,28 @@ import time
 from libraries.IO import saveData
 from libraries.lifeDist import lifeDist, lifeDist2
 
-#Generate a point
+
+# Generate a point
 def getPoint(maxL=10):
-    RStarSample = random.uniform(0 , 2) #logaritmic
-    fPlanets = random.uniform(-1 , 0)
-    nEnvironment = random.uniform(-1 , 0)
-    fInteligence = random.uniform(-3 , 0)
-    fCivilization = random.uniform(-2 , 0)
+    RStarSample = random.uniform(0, 2)  # logaritmic
+    fPlanets = random.uniform(-1, 0)
+    nEnvironment = random.uniform(-1, 0)
+    fIntelligence = random.uniform(-3, 0)
+    fCivilization = random.uniform(-2, 0)
 
-    #L = np.random.uniform(2 , maxL)                         #loguniform
-    #L = np.random.normal(2, maxL)                          #lognormal
-    L = math.log10(np.random.uniform(10**2 , 10**maxL))    #uniform
-    #...    #gauss
+    # L = np.random.uniform(2 , maxL)                         #loguniform
 
-    #fLife = random.uniform(-2 , 0)
-    #fLifeEks = fLife
+    # median = (maxL - 2) / 2 + 2  # polovica intervala
+    # sigma = (median - 2) / 3  # tako, da je 3sigma cez cel obseg
+    # L = np.random.normal(median, sigma)  # lognormal
+
+    # L = math.log10(np.random.uniform(10 ** 2, 10 ** maxL))  # uniform
+
+    sigmaHalfGauss = (10 ** maxL - 10 ** 2) / 6 # interval/2 je polovic in se /3 je tako da bo 3sigma cez cel interval
+    L = np.log10(np.abs(np.random.normal(0, sigmaHalfGauss)) + 10 ** 2)
+
+    # fLife = random.uniform(-2 , 0)
+    # fLifeEks = fLife
     nStars = random.uniform(11, 11.60205999132)
 
     fLife = lifeDist(mean=0, sigma=50)
@@ -31,31 +38,32 @@ def getPoint(maxL=10):
 
     E3 = nStars + fPlanets + nEnvironment
     E4 = E3 + fLife
-    E5 = E4 + fInteligence
+    E5 = E4 + fIntelligence
 
+    resitev = RStarSample + fPlanets + nEnvironment + fLifeEks + fIntelligence + fCivilization + L  # calculates N
 
-    resitev = RStarSample + fPlanets + nEnvironment + fLifeEks + fInteligence + fCivilization + L   #calculates N
-
-    #thresholds
-    if (resitev<0) or E4 < math.log(2,10) or E3 < math.log(3,10) or resitev > 5:
-        return getPoint(maxL)
+    # thresholds
+    if (resitev < 0) or E4 < math.log(2, 10) or E3 < math.log(3, 10) or resitev > 5:
+        # return getPoint(maxL)
+        return False
 
     return resitev
 
-#devide Lmax scale
-numHorSec = 48  #on how many parts should we divide L - how many different Lmax should we take
-noIterationsPerMaxL = 30000     #How many points N per each file/maxL - should be as big as possible  ##trenutno 1000, da jih hitrej generira
-logPoints = np.linspace(2,10,numHorSec) #devide on numHorSec equal parts a scale from 2 to 10 -
 
-#for each Lmax create a file with points
+# divide Lmax scale
+numHorSec = 48  # on how many parts should we divide L - how many different Lmax should we take
+noIterationsPerMaxL = 30000  # How many points N per each file/maxL - should be as big as possible  ##trenutno 1000, da jih hitreje generira
+logPoints = np.linspace(2, 10, numHorSec)  # devide on numHorSec equal parts a scale from 2 to 10 -
+
+# for each Lmax create a file with points
 for maxL in logPoints:
-    array=[]
+    array = []
     for _ in range(0, noIterationsPerMaxL):
         point = getPoint(maxL)
         if type(point) != type(False):
             array.append(point)
 
-    saveData(array,"/inf"+str(maxL))
-    print("File: inf"+str(maxL)+".txt created")
+    saveData(array, "/inf" + str(maxL))
+    print("File: inf" + str(maxL) + ".txt created. Size: " + str(len(array)))
 
 print('done')
--- plot3D_N(L).py
+++ plot3D_N(L).py
@@ -1,4 +1,4 @@
-from libraries.IO import  readData
+from libraries.IO import readData
 from libraries.IO import saveData
 from matplotlib import cm
 import matplotlib.pyplot as plt
@@ -7,22 +7,22 @@ from mpl_toolkits.mplot3d import Axes3D
 import scipy.ndimage.filters as fl
 from libraries.StandardizeDistribution import StandardizeDistributionW
 
-
 numHorSec = 48
 noBins = 50
-bins = np.linspace(-13,20,noBins+1) #devide N scale
-horSec = np.linspace(2,10,numHorSec)    #devide Lmax scale
-Z = [None]*numHorSec    #no.hits 2D array - we draw this
-i=0
+bins = np.linspace(-3, 6, noBins + 1)  # devide N scale
+horSec = np.linspace(2, 10, numHorSec)  # devide Lmax scale
+Z = [None] * numHorSec  # no.hits 2D array - we draw this
+i = 0
 
-#go through the files
+# go through the files
 for fileNo in horSec:
-    array = readData("/inf"+str(fileNo))
-    Z[i],_ = np.histogram(array, bins)
-    #Z[i] = np.multiply(Z[i], 2.0/float(fileNo), out=Z[i], casting="unsafe")    # decay factor is 2/fileNo = 2/maxL
-    i+=1
-   
-X, Y = np.meshgrid(bins[0:-1],horSec)
+    array = readData("/inf" + str(fileNo))
+    # array = array[0:4664]
+    Z[i], _ = np.histogram(array, bins)
+    # Z[i] = np.multiply(Z[i], 2.0/float(fileNo), out=Z[i], casting="unsafe")    # decay factor is 2/fileNo = 2/maxL
+    i += 1
+
+X, Y = np.meshgrid(bins[0:-1], horSec)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -31,13 +31,13 @@ X = np.array(X)
 Y = np.array(Y)
 Z = np.array(Z)
 
-#PRINT Z aka no_hits table used for other programs
+# PRINT Z aka no_hits table used for other programs
 np.savetxt('no_hits.txt', Z, fmt='%.0f')
-#saveData(Z, "no_hits")   old
+# saveData(Z, "no_hits")   old
 print("Saved no. of hits")
 
 # Plot the surface
-ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,shade=True,color='b',alpha=0.8)
+ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, shade=True, color='b', alpha=0.8)
 
 ax.set_xlabel("log(N)")
 ax.set_ylabel("log(maxL)")
